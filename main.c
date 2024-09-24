224ebf74f8ba9cc9fd7ac767fcaccfed5fe35fc0
Dima Patyk <patyk06@mail.ru>
2024-09-24 15:40:55 +0300

    Continue

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 
 
 int main(void) {
-    printf("Hello world!\n");
+    int a, b;
+    printf("Введите первое число");
+    scanf("%d", &a);
+    printf("Введите второе число");
+    scanf("%d", &b);
 
     return 0;
 }
